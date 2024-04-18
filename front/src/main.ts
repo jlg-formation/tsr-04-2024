b5922d063598a6e42d7f1797796c3018f0664ee2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-04-18 17:24:03 +0200

    dev: svg ok

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,24 +1,59 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const r = 1;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const cx0 = 50;
+const cy0 = 50;
+const r0 = 45;
+
+const gSamples = document.querySelector(".samples");
+if (gSamples === null) {
+  throw new Error("Cannot find selector .samples");
+}
+
+const samples = 10;
+const multiplicationFactor = 2;
+for (let i = 0; i < samples; i++) {
+  console.log("i: ", i);
+
+  const angle = (i * 2 * Math.PI) / samples - Math.PI / 2;
+
+  const cx = cx0 + r0 * Math.cos(angle);
+  const cy = cy0 + r0 * Math.sin(angle);
+
+  const circle = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "circle"
+  );
+  circle.setAttribute("cx", cx + "");
+  circle.setAttribute("cy", cy + "");
+  circle.setAttribute("r", r + "");
+  gSamples.appendChild(circle);
+}
+
+const gLines = document.querySelector(".lines");
+if (gLines === null) {
+  throw new Error("Cannot find selector .lines");
+}
+
+for (let i = 0; i < samples; i++) {
+  console.log("i: ", i);
+
+  const angle1 = (i * 2 * Math.PI) / samples - Math.PI / 2;
+
+  const x1 = cx0 + r0 * Math.cos(angle1);
+  const y1 = cy0 + r0 * Math.sin(angle1);
+
+  const angle2 =
+    (i * multiplicationFactor * 2 * Math.PI) / samples - Math.PI / 2;
+
+  const x2 = cx0 + r0 * Math.cos(angle2);
+  const y2 = cy0 + r0 * Math.sin(angle2);
+
+  const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+  line.setAttribute("x1", x1 + "");
+  line.setAttribute("y1", y1 + "");
+  line.setAttribute("x2", x2 + "");
+  line.setAttribute("y2", y2 + "");
+  gLines.appendChild(line);
+}
