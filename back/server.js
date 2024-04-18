32956b39e2028b0db66737a15cc77bf0a9b538c0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-04-18 12:34:46 +0200

    dev: added express api router

--- back/server.js
+++ back/server.js
@@ -3,6 +3,8 @@ console.log("About to start a server...");
 const express = require("express");
 const serveIndex = require("serve-index");
 
+const api = require("./api");
+
 const app = express();
 const port = 3000;
 const publicDir = ".";
@@ -11,6 +13,8 @@ app.use((req, res, next) => {
   console.log("req: ", req.method, req.url);
   next();
 });
+
+app.use("/api", api);
 
 app.use(express.static(publicDir));
 app.use(serveIndex(publicDir, { icons: true }));
