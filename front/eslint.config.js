2ba2f28822f64e2bb3dc422b95de0a4e8ac8624d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-04-19 09:56:50 +0200

    dev: added eslint

--- front/eslint.config.js
+++ front/eslint.config.js
@@ -2,9 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: globals.browser },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      eqeqeq: ["error"],
+    },
+  },
 ];
