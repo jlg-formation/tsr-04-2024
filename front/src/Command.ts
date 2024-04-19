7e763770ebdc103eea7917eaf06b400679624283
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-04-19 15:18:47 +0200

    dev: added action

--- front/src/Command.ts
+++ front/src/Command.ts
@@ -9,6 +9,7 @@ export class Command {
   constructor(private config: Config) {
     console.log("config: ", this.config);
     this.render();
+    this.setActions();
   }
 
   render(): void {
@@ -24,6 +25,23 @@ export class Command {
       );
       console.log("sliderElt: ", sliderElt);
       sliderElt.value = this.config[key] + "";
+    }
+  }
+
+  setActions(): void {
+    const array = keys(this.config);
+    for (const key of array) {
+      const sliderElt = querySelector(
+        `div.command label.${key} input`,
+        HTMLInputElement,
+      );
+
+      sliderElt.addEventListener("input", () => {
+        console.log("input");
+        this.config[key] = Number(sliderElt.value);
+        this.render();
+        this.callback(this.config);
+      });
     }
   }
 
